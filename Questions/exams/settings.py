23880b0c2588ab961bebbc8a4bc74a16b8769d20
alexdeitos <alexdeitos01@gmail.com>
2023-08-14 19:59:05 -0300

    commit das 205 perguntas com sucesso

--- Questions/exams/settings.py
+++ Questions/exams/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1v+1*izb+x%(zz2y700rg$j+s0$e#6p=@mg*$r6%zcg+&@^1%z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.100']
+ALLOWED_HOSTS = ['192.168.100.100','127.0.0.1']
 
 # Application definition
 
