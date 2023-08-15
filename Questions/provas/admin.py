ee33dc4ef160f31b7b12dc53c840eed81f6a7bb5
alexdeitos <alexdeitos01@gmail.com>
2023-08-15 11:52:19 -0300

    last commit - ajustes css

--- Questions/provas/admin.py
+++ Questions/provas/admin.py
@@ -7,7 +7,27 @@ class RespostaInline(admin.TabularInline):
 
 class PerguntaAdmin(admin.ModelAdmin):
     inlines = [RespostaInline]
-#    ordering = ['id']  # Ordena pelo campo 'id', que representa a ordem de criação
+    
+    # Adicione uma barra de pesquisa para procurar pelo ID da pergunta
+    search_fields = ['id', 'texto']
+    
+    # Liste as perguntas com os campos ID e texto da pergunta
+    list_display = ['id', 'texto']
 
 admin.site.register(Pergunta, PerguntaAdmin)
 
+
+"""
+from django.contrib import admin
+from .models import Pergunta, Resposta
+
+class RespostaInline(admin.TabularInline):
+    model = Resposta
+    extra = 4
+
+class PerguntaAdmin(admin.ModelAdmin):
+    inlines = [RespostaInline]
+#    ordering = ['id']  # Ordena pelo campo 'id', que representa a ordem de criação
+
+admin.site.register(Pergunta, PerguntaAdmin)
+"""
