1b02f97d970930f473253a96328e4372086299a3
alexdeitos <alexdeitos01@gmail.com>
2024-03-19 18:48:42 -0300

    ajuste site

--- Questions/provas/static/provas/script.js
+++ Questions/provas/static/provas/script.js
@@ -1,45 +1,91 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('enviar-respostas').addEventListener('click', function() {
-        marcarRespostas();
+    const scrollToBottomBtn = document.getElementById('scroll-to-bottom');
+    scrollToBottomBtn.addEventListener('click', () => {
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+    const scrollToTopBtn = document.getElementById('scroll-to-top');
+    scrollToTopBtn.addEventListener('click', () => {
+        window.scrollTo(0, 0);
+    });
+    const UpdateFuture = document.getElementById('future');
+    UpdateFuture.addEventListener('click', () => {
+        window.location.reload();
     });
 
-    document.getElementById('limpar-respostas').addEventListener('click', function() {
-        limparRespostas();
-    });
+    let perguntasCorretas = 0;
+    let perguntasIncorretas = 0;
+    let perguntasNaoRespondidas = 0;
 
-    function marcarRespostas() {
-        // Loop sobre todas as perguntas
-        document.querySelectorAll('.pergunta').forEach(function(pergunta) {
-            // Loop sobre todas as respostas da pergunta
-            pergunta.querySelectorAll('.resposta').forEach(function(resposta) {
-                var checkbox = resposta.querySelector('input[type="checkbox"]');
-                var respostaLabel = resposta.querySelector('.resposta-label');
+    const enviarRespostasBtn = document.getElementById('enviar-respostas');
+    enviarRespostasBtn.addEventListener('click', validarRespostas);
 
-                // Remover classes anteriores
-                resposta.classList.remove('resposta-correta', 'resposta-incorreta');
+    const limparRespostasBtn = document.getElementById('limpar-respostas');
+    limparRespostasBtn.addEventListener('click', limparRespostas);
 
-                // Verifica se a resposta está correta
-                if (checkbox.checked) {
-                    if (checkbox.getAttribute('data-correta') === 'true') {
-                        resposta.classList.add('resposta-correta');
-                    } else {
-                        resposta.classList.add('resposta-incorreta');
-                    }
-                }
-            });
+    const marcarCorretasBtn = document.getElementById('marcar-corretas');
+    marcarCorretasBtn.addEventListener('click', marcarRespostasCorretas);
+
+    function marcarRespostasCorretas() {
+        const respostas = document.querySelectorAll('input[type="checkbox"]');
+        respostas.forEach(resposta => {
+            const perguntaId = resposta.name;
+            const respostaLabel = document.getElementById(`resposta-label-${perguntaId}-${resposta.value}`);
+            if (resposta.getAttribute('data-correta')) {
+                resposta.checked = true;
+                respostaLabel.style.color = 'blue';
+            }
         });
     }
 
     function limparRespostas() {
-        // Desmarcar todos os checkboxes
-        document.querySelectorAll('#formulario-respostas input[type="checkbox"]').forEach(function(checkbox) {
-            checkbox.checked = false;
-        });
+        perguntasCorretas = 0;
+        perguntasIncorretas = 0;
+        perguntasNaoRespondidas = 0;
 
-        // Remover classes de cor
-        document.querySelectorAll('.resposta').forEach(function(resposta) {
-            resposta.classList.remove('resposta-correta', 'resposta-incorreta');
+        const respostas = document.querySelectorAll('input[type="checkbox"]');
+        respostas.forEach(resposta => {
+            resposta.checked = false;
+            const perguntaId = resposta.name;
+            const respostaLabel = document.getElementById(`resposta-label-${perguntaId}-${resposta.value}`);
+            respostaLabel.style.color = '';
         });
     }
-});
 
+    function validarRespostas() {
+        perguntasNaoRespondidas = 0;
+        perguntasCorretas = 0;
+        perguntasIncorretas = 0;
+
+        const totalPerguntas = document.querySelectorAll('.pergunta').length;
+        const respostasMarcadas = document.querySelectorAll('input[type="checkbox"]:checked');
+
+        respostasMarcadas.forEach(resposta => {
+            const perguntaId = resposta.name;
+            const respostaLabel = document.getElementById(`resposta-label-${perguntaId}-${resposta.value}`);
+
+            if (resposta.getAttribute('data-correta')) {
+                respostaLabel.style.color = 'blue'; // Marca a resposta como correta (azul)
+                perguntasCorretas++;
+            } else {
+                respostaLabel.style.color = 'red'; // Marca a resposta como incorreta (vermelha)
+                perguntasIncorretas++;
+                const respostasDaPergunta = document.querySelectorAll(`input[name="${perguntaId}"]`);
+                respostasDaPergunta.forEach(respostaCorreta => {
+                    if (respostaCorreta.getAttribute('data-correta')) {
+                        const respostaCorretaLabel = document.getElementById(`resposta-label-${perguntaId}-${respostaCorreta.value}`);
+                        respostaCorretaLabel.style.color = 'blue'; // Marca a resposta correta como azul
+                    }
+                });
+            }
+        });
+
+        perguntasNaoRespondidas = totalPerguntas - respostasMarcadas.length;
+        const mensagem = `Você respondeu corretamente ${perguntasCorretas} pergunta(s).\n`
+            + `Você respondeu incorretamente ${perguntasIncorretas} pergunta(s).\n`
+            + `Você não respondeu ${perguntasNaoRespondidas} pergunta(s).`;
+        alert(mensagem);
+
+        // Marca as respostas corretas após o usuário ter visto o alerta
+        marcarRespostasCorretas();
+    }
+});
